9608a351cb67c92e23b29beae6f04fbd5d8907b6
Michael Ries <michael.ries@gmail.com>
2017-04-12 21:08:01 +0000

    add labels for items in details listing

--- lib/beer_advocate/cli.rb
+++ lib/beer_advocate/cli.rb
@@ -32,10 +32,11 @@ class BeerAdvocate::CLI
   end
 
   def self.show_beer_details(beer)
-    puts "#{beer.name}"
-    puts "#{beer.brewery}"
-    puts "#{beer.style}, #{beer.abv}"
-    puts "#{beer.description}"
+    puts ""
+    puts "Name: #{beer.name}"
+    puts "Brewed by: #{beer.brewery}"
+    puts "Style: #{beer.style} #{beer.abv}"
+    puts "Description: #{beer.description}"
     puts ""
     puts "Please enter 'list' to review the top-ten, or type 'exit'."
   end
