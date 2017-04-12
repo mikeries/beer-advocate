9b4d8a91314e2f2afd9a9df528b3f730706390aa
Michael Ries <michael.ries@gmail.com>
2017-04-12 19:01:08 +0000

    Scraper#load_beers scrapes beer names

--- lib/beer_advocate/beer.rb
+++ lib/beer_advocate/beer.rb
@@ -15,11 +15,11 @@ class BeerAdvocate::Beer
 
   def details
     self.description = "This beer was conceived with the intent to push the concept of Doppelganger to 
-    the limit of flavor and intensity. Both the kettle hopping rates and dry hopping 
-    rates were increased while keeping the base beer the same. The result is intense, 
-    but also surprising in its balance and softness. The mouthfeel is viscous and 
-    coating with flavors of overripe mango, dank citrus, and tropical fruit 
-    balanced by a sharp but pleasant finish. A treat to warm you up as a true New 
+    the limit of flavor and intensity. Both the kettle hopping rates and dry hopping
+    rates were increased while keeping the base beer the same. The result is intense,
+    but also surprising in its balance and softness. The mouthfeel is viscous and
+    coating with flavors of overripe mango, dank citrus, and tropical fruit
+    balanced by a sharp but pleasant finish. A treat to warm you up as a true New
     England winter takes hold!"
   end
 
--- lib/beer_advocate/scraper.rb
+++ lib/beer_advocate/scraper.rb
@@ -4,6 +4,15 @@ require 'pry'
 class BeerAdvocate::Scraper
   def self.scrape_beers
     doc = Nokogiri::HTML(open('https://www.beeradvocate.com/lists/top/'))
-    binding.pry
+    beers = doc.css('#ba-content table tr')
+    counter = 0;
+    name=[]
+    beers.each do |beer|
+      counter+=1
+      if counter > 2 && counter < 12
+        name<<beer.children[1].children[0].children[0].children[0].text
+      end
+    end
+    name
   end
 end
