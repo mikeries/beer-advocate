782fa2fdd68ebb5561aa066c02cb11d3cabd8f6b
Michael Ries <michael.ries@gmail.com>
2017-04-12 19:20:47 +0000

    added remaining properties to scrape from main page

--- lib/beer_advocate/scraper.rb
+++ lib/beer_advocate/scraper.rb
@@ -9,9 +9,14 @@ class BeerAdvocate::Scraper
     results=[]
     beers.each do |beer|
       counter+=1
-      if counter > 2 && counter < 12
+      if counter > 2 && counter < 4
         beer_hash = {
-          name: beer.children[1].children[0].children[0].children[0].text
+          name: beer.children[1].children[0].children[0].children[0].text,
+          brewery: beer.children[1].children[1].children[0].text,
+          style: beer.children[1].children[1].children[2].text,
+          abv: beer.children[1].children[1].children[3].text,
+          rank: counter-2,
+          url: beer.children[1].children[0].attribute("href").value
         }
         results << beer_hash
         binding.pry
