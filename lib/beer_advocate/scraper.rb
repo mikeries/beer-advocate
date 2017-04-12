0eb9b5ce20e63d8e4f89d2c4176edd5a0dce97c0
Michael Ries <michael.ries@gmail.com>
2017-04-12 19:05:50 +0000

    Scraper#scrape_beers returns a hash containing beer properties

--- lib/beer_advocate/scraper.rb
+++ lib/beer_advocate/scraper.rb
@@ -6,13 +6,16 @@ class BeerAdvocate::Scraper
     doc = Nokogiri::HTML(open('https://www.beeradvocate.com/lists/top/'))
     beers = doc.css('#ba-content table tr')
     counter = 0;
-    name=[]
+    results=[]
     beers.each do |beer|
       counter+=1
       if counter > 2 && counter < 12
-        name<<beer.children[1].children[0].children[0].children[0].text
+        beer_hash = {
+          name: beer.children[1].children[0].children[0].children[0].text
+        }
+        results << beer_hash
+        binding.pry
       end
     end
-    name
   end
 end
