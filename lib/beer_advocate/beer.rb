0926b1fd9227c16f867ac9a8996d137d9bbf0caf
Michael Ries <michael.ries@gmail.com>
2017-04-12 19:41:27 +0000

    Beer#all gets array of hashes from scraper and creates beers using #create_beer_from_hash

--- lib/beer_advocate/beer.rb
+++ lib/beer_advocate/beer.rb
@@ -9,12 +9,17 @@ class BeerAdvocate::Beer
     self
   end
 
+  def self.create_beer_from_hash(beer_hash)
+    beer = BeerAdvocate::Beer.new(beer_hash[name]);
+    beer_hash.each {|key, value| beer.send(("#{key}="), value)}
+  end
+
   def save
     @@all << self
   end
 
   def details
-    self.description = "This beer was conceived with the intent to push the concept of Doppelganger to 
+    self.description = "This beer was conceived with the intent to push the concept of Doppelganger to
     the limit of flavor and intensity. Both the kettle hopping rates and dry hopping
     rates were increased while keeping the base beer the same. The result is intense,
     but also surprising in its balance and softness. The mouthfeel is viscous and
@@ -25,19 +30,7 @@ class BeerAdvocate::Beer
 
   def self.all
     if @@all.count == 0
-      beer = BeerAdvocate::Beer.new("Doubleganger")
-      beer.style = "American Double / Imperial IPA"
-      beer.rating = 4.61
-      beer.brewery = "Tree House Brewing Company"
-      beer.abv = "8.20% ABV"
-      beer.rank = 1
-
-      beer = BeerAdvocate::Beer.new("Curiousity Twenty Seven")
-      beer.style = "American Double / Imperial IPA"
-      beer.rating = 4.53
-      beer.brewery = "Tree House Brewing Company"
-      beer.abv = "8.00% ABV"
-      beer.rank = 2
+      BeerAdvocate::Scraper.scrape_beers.each {|beer| self.create_beer_from_hash(beer)}
     end
 
     @@all
