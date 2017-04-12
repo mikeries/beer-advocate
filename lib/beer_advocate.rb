1b48a396049b588b12d4881f592d72422b98c75b
Michael Ries <michael.ries@gmail.com>
2017-04-12 15:32:46 +0000

    add cli.rb and dependency

--- lib/beer_advocate.rb
+++ lib/beer_advocate.rb
@@ -1,2 +1,2 @@
 require "beer_advocate/version"
-
+require "beer_advocate/cli"
---
+++ lib/beer_advocate/cli.rb
@@ -0,0 +1,7 @@
+class BeerAdvocate::CLI
+
+  def self.start
+    puts "World's Best Beers, as rated by BeerAdvocate.com"
+  end
+
+end
