9b165172d10641d1e6d4b62361ffe2322e520035
Michael Ries <michael.ries@gmail.com>
2017-04-13 15:02:33 +0000

    add sample result for text in #scrape_description

--- lib/beer_advocate/scraper.rb
+++ lib/beer_advocate/scraper.rb
@@ -28,6 +28,21 @@ class BeerAdvocate::Scraper
   def self.scrape_description(url)
     doc = Nokogiri::HTML(open(ROOT_URL+url))
     text = doc.css('#ba-content > div:nth-child(4) > div:nth-child(2)').text
+
+    # sample result for text:
+    #  "\n\t\t\n\t\t\t\t\n\n\t\tBEER INFO\n\n\t\t\n\t\tBrewed by:\n\t\t\n\t\tTree
+    #  House Brewing Company\n\t\tMassachusetts, United Statestreehousebrew.com\t\t\n\t\t
+    #  Style: American Double / Imperial IPA\n\t\t\n\t\tAlcohol by v
+    #  olume (ABV): 8.60%\n\t\t\n\t\tAvailability: Rotating\n\t\t\n\t\tNotes /
+    #  Commercial Description:\n\t\t\n\t\tA kicked up version of Haze, Very Hazy
+    #  is a beer that makes us Very Happy! Very Hazy conveys all the beautiful fla
+    #  vors of Haze, but with even greater depth and potency. Pungent grapefruit
+    #  notes greet your palate while an underlying current of soft tropical fruit
+    #  dances in the background. Just the right amount of bitterness balances t
+    #  he sweet fruit flavors while a velvety soft mouthfeel make you easily (and
+    #  dangerously!) forget this one clocks in at 8.6% ABV. A huge and beautiful 
+    #  beer that maintains softness and delicacy - a beer we are very excited a
+    #  bout!Added by ThatsThatDude on 11-22-2014\t\t\n\t"
     description = text.split("\n")[19].split(/(.*)Added by.*/)[1].strip
   end
 end
