b04e4c0fd2e7cd3e0941640bdd00909c735239d4
Michael Ries <michael.ries@gmail.com>
2017-04-13 14:54:15 +0000

    refactor element variable to row, strip "Added by" from description

--- lib/beer_advocate/scraper.rb
+++ lib/beer_advocate/scraper.rb
@@ -7,24 +7,24 @@ class BeerAdvocate::Scraper
     doc = Nokogiri::HTML(open(ROOT_URL+'/lists/top/'))
     rows = doc.css('#ba-content table tr')
 
-    results = (3..12).collect {|index| parse_element(rows[index-1])}
+    results = (3..12).collect {|index| parse_row(rows[index-1])}
   end
 
-  def self.parse_element(element)
+  def self.parse_row(row)
     {
-      name: element.children[1].children[0].children[0].children[0].text,
-      brewery: element.children[1].children[1].children[0].text,
-      style: element.children[1].children[1].children[2].text,
-      abv: element.children[1].children[1].children[3].text,
-      rank: element.children[0].text,
-      url: element.children[1].children[0].attribute("href").value,
-      rating: element.children[2].children[0].text
+      name: row.children[1].children[0].children[0].children[0].text,
+      brewery: row.children[1].children[1].children[0].text,
+      style: row.children[1].children[1].children[2].text,
+      abv: row.children[1].children[1].children[3].text,
+      rank: row.children[0].text,
+      url: row.children[1].children[0].attribute("href").value,
+      rating: row.children[2].children[0].text
     }
   end
 
   def self.scrape_description(url)
     doc = Nokogiri::HTML(open(ROOT_URL+url))
     text = doc.css('#ba-content > div:nth-child(4) > div:nth-child(2)').text
-    description = text.split("\n")[19].strip
+    description = text.split("\n")[19].split(/(.*)Added by.*/)[1].strip
   end
 end
